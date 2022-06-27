5123bbf1bf4c0356e80b470c85d4c7942394fd59
Ahmet Meydan <asmeydan0816@gmail.com>
2022-06-27 22:36:15 +0300

    bugs fixed and the edges are working

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const input = document.querySelectorAll("input");
 const finish = document.getElementById("finish");
 const win = document.getElementById("win");
 const lose = document.getElementById("lose");
+const btn = document.querySelectorAll("button");
 
 
 
@@ -24,12 +25,48 @@ container.addEventListener("click",(e)=> {
     if(element.matches("div.cover")) {
         cover[elementId-1].style.display = "none";
 
-        if(map[elementId] == "0" && elementId<70 && elementId>10 && !(elementId%10 == 1) && !(elementId%10 == 0)) {
+        if(map[elementId] == "0") {
             //console.log("boşa bastın")
-            around.push(elementId-11,elementId-10,elementId-9,elementId-1,
-                parseInt(elementId)+1,parseInt(elementId)+9,parseInt(elementId)+10,parseInt(elementId)+11)
+            if(elementId<70 && elementId>10 && !(elementId%10 == 1) && !(elementId%10 == 0)){
+                around.push(elementId-11,elementId-10,elementId-9,elementId-1,
+                    parseInt(elementId)+1,parseInt(elementId)+9,
+                    parseInt(elementId)+10,parseInt(elementId)+11)
+            }
+            if((elementId == 1)){
+                around.push(parseInt(elementId)+1, parseInt(elementId)+10, parseInt(elementId)+11)
+            }
+            if((elementId == 10)){
+                around.push(elementId-1, parseInt(elementId)+9, parseInt(elementId)+10)
+            }
+            if((elementId == 71)){
+                around.push(elementId-10, elementId-9, parseInt(elementId)+1)
+            }
+            if((elementId == 80)){
+                around.push(elementId-11, elementId-10, elementId-1)
+            }
+            //? TOP
+            if((elementId > 1) && (elementId < 10)){
+                around.push(elementId-1, parseInt(elementId)+1, parseInt(elementId)+9,
+                 parseInt(elementId)+10, parseInt(elementId)+11)
+            }
+            //? RİGHT
+            if(!(elementId == 10) && !(elementId == 80) && (elementId%10 == 0)){
+                around.push(elementId-11, elementId-10, elementId-1,
+                 parseInt(elementId)+9, parseInt(elementId)+10)
+            }
+            //? LEFT
+            if(!(elementId == 1) && !(elementId == 71) && (elementId%10 == 1)){
+                around.push(elementId-10, elementId-9, parseInt(elementId)+1,
+                 parseInt(elementId)+10, parseInt(elementId)+11)
+            }
+            //? BOTTOM
+            if((elementId > 71) && (elementId < 80)){
+                around.push(elementId-11, elementId-10, elementId-9,
+                    elementId-1, parseInt(elementId)+1)
+            }
             
-            for(let i = 0; i<8; i++) {
+            
+            for(let i = 0; i<15; i++) {
                 around.forEach((e)=> {
                     if(map[e] == "0" && e<70 && e>10 && !(e%10 == 1) && !(e%10 == 0)) {
                         //! around.push(e-11,e-10,e-9,e-1,parseInt(e)+1,parseInt(e)+9,parseInt(e)+10,parseInt(e)+11)
@@ -58,6 +95,126 @@ container.addEventListener("click",(e)=> {
                             around.push(parseInt(e)+11)
                         }
                     }
+                    //? SOL ÜST KÖŞE
+                    if(map[e] == "0" && (e == 1)){
+                        if(!around.includes(parseInt(e)+1)) {
+                            around.push(parseInt(e)+1)
+                        }
+                        if(!around.includes(parseInt(e)+10)) {
+                            around.push(parseInt(e)+10)
+                        }
+                        if(!around.includes(parseInt(e)+11)) {
+                            around.push(parseInt(e)+11)
+                        }
+                    }
+                    //? SOL ALT KÖŞE
+                    if(map[e] == "0" && (e == 71)){
+                        if(!around.includes(parseInt(e)+1)) {
+                            around.push(parseInt(e)+1)
+                        }
+                        if(!around.includes(parseInt(e)-10)) {
+                            around.push(parseInt(e)-10)
+                        }
+                        if(!around.includes(parseInt(e)-9)) {
+                            around.push(parseInt(e)-9)
+                        }
+                    }
+                    //? SAĞ ALT KÖŞE
+                    if(map[e] == "0" && (e == 80)){
+                        if(!around.includes(parseInt(e)-11)) {
+                            around.push(parseInt(e)-11)
+                        }
+                        if(!around.includes(parseInt(e)-10)) {
+                            around.push(parseInt(e)-10)
+                        }
+                        if(!around.includes(parseInt(e)-1)) {
+                            around.push(parseInt(e)-1)
+                        }
+                    }
+                    //? SAĞ ÜST KÖŞE
+                    if(map[e] == "0" && (e == 10)){
+                        if(!around.includes(parseInt(e)-1)) {
+                            around.push(parseInt(e)-1)
+                        }
+                        if(!around.includes(parseInt(e)+9)) {
+                            around.push(parseInt(e)+9)
+                        }
+                        if(!around.includes(parseInt(e)+10)) {
+                            around.push(parseInt(e)+10)
+                        }
+                    }
+                    //?  ÜST KENAR
+                    if(map[e] == "0" && (e > 1) && (e < 10)){
+                        if(!around.includes(parseInt(e)-1)) {
+                            around.push(parseInt(e)-1)
+                        }
+                        if(!around.includes(parseInt(e)+1)) {
+                            around.push(parseInt(e)+1)
+                        }
+                        if(!around.includes(parseInt(e)+9)) {
+                            around.push(parseInt(e)+9)
+                        }
+                        if(!around.includes(parseInt(e)+10)) {
+                            around.push(parseInt(e)+10)
+                        }
+                        if(!around.includes(parseInt(e)+11)) {
+                            around.push(parseInt(e)+11)
+                        }
+                    }
+                    //?  SOL KENAR
+                    if(map[e] == "0" && (e%10 == 1) && !(e == 71) && !(e == 1)){
+                        if(!around.includes(parseInt(e)-10)) {
+                            around.push(parseInt(e)-10)
+                        }
+                        if(!around.includes(parseInt(e)-9)) {
+                            around.push(parseInt(e)-9)
+                        }
+                        if(!around.includes(parseInt(e)+1)) {
+                            around.push(parseInt(e)+1)
+                        }
+                        if(!around.includes(parseInt(e)+10)) {
+                            around.push(parseInt(e)+10)
+                        }
+                        if(!around.includes(parseInt(e)+11)) {
+                            around.push(parseInt(e)+11)
+                        }
+                    }
+                    //?  SAĞ KENAR
+                    if(map[e] == "0" && (e%10 == 0) && !(e == 10) && !(e == 80)){
+                        if(!around.includes(parseInt(e)-11)) {
+                            around.push(parseInt(e)-11)
+                        }
+                        if(!around.includes(parseInt(e)-10)) {
+                            around.push(parseInt(e)-10)
+                        }
+                        if(!around.includes(parseInt(e)-1)) {
+                            around.push(parseInt(e)-1)
+                        }
+                        if(!around.includes(parseInt(e)+10)) {
+                            around.push(parseInt(e)+10)
+                        }
+                        if(!around.includes(parseInt(e)+9)) {
+                            around.push(parseInt(e)+9)
+                        }
+                    }
+                    //?  ALT KENAR
+                    if(map[e] == "0" && (e < 80) && (e > 71)){
+                        if(!around.includes(parseInt(e)-11)) {
+                            around.push(parseInt(e)-11)
+                        }
+                        if(!around.includes(parseInt(e)-10)) {
+                            around.push(parseInt(e)-10)
+                        }
+                        if(!around.includes(parseInt(e)-9)) {
+                            around.push(parseInt(e)-9)
+                        }
+                        if(!around.includes(parseInt(e)-1)) {
+                            around.push(parseInt(e)-1)
+                        }
+                        if(!around.includes(parseInt(e)+1)) {
+                            around.push(parseInt(e)+1)
+                        }
+                    }
                 })
             }
             
@@ -82,3 +239,11 @@ for(let i = 0; i<input.length; i++) {
         input[i].value = "";
     }
 }
+
+
+btn[0].addEventListener("click", ()=> {
+    location.reload();
+})
+btn[1].addEventListener("click", ()=> {
+    location.reload();
+})
